8bc35545cd4f0eab87f9f4718cf0bc72aaf624bf
procollision <43912632+procollision@users.noreply.github.com>
2020-09-15 08:50:53 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,10 +1,13 @@
 import tradeoff_class as tc
 #Define paramters in tradeoff
-#Name is the display name used in table printing
-#weight is the assigned weight for the parameter
-#Limitype is how the range of values that will give map top [0,1]. there are three options "fixed" the rnage is manually defined,
-#"minmax" the range is defined such that the worst option is 0 and the best is 1, "SD" the range is given as mean+-x*standard deviation
-#limitype is only nesscarry for "fixed" where it defines the manual rangee, and "SD" where it defines x
+#list of arguments:
+# Name is the display name used in table printing
+# weight is the assigned weight for the parameter
+# func (default "LRTS") can either be LRTS, IRTS or DRTS see tutorial page in notion for description
+# direc (default "HB") either "HB" for high is best or "LB" for Low is best
+# Limitype (default "minmax") is how the range of values that will give map top [0,1]. there are three options "fixed" the rnage is manually defined,
+# "minmax" the range is defined such that the worst option is 0 and the best is 1, "SD" the range is given as mean+-x*standard deviation
+# limitype is only nesscarry for "fixed" where it defines the manual rangee, and "SD" where it defines x
 avi = tc.param(name="Availability",weight=1/3,Limitype ="fixed",Limit_val=[1,5])
 acc = tc.param(name="Accuracy",weight=1/3,Limitype ="fixed",Limit_val=[1,5])
 sam = tc.param(name="sample rate",weight=1/3,Limitype ="fixed",Limit_val=[1,5])
@@ -19,6 +22,7 @@ colors = [tc.color("EF5350", "red"), tc.color("FB8C00", "orange"), tc.color("FFE
 tradeoff_att =tc.tradeoff(design_list = [gyro,sun,star,maf],param_list= [avi,acc,sam])
 
 tradeoff_att.get_tradeoff()
+
 tradeoff_att.get_output(language="latex",color_list=colors,width=10,rot="hor",caption="Tradeoff Rotational sensors")
 
 
